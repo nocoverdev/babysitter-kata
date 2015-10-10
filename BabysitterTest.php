3216d5870bcae6904a895213a87f1772946e2489
nocoverdev <nocoverdev@gmail.com>
2015-10-09 20:44:58 -0500

    refactored testing code

--- BabysitterTest.php
+++ BabysitterTest.php
@@ -6,19 +6,24 @@ require_once 'Babysitter.php';
 
 class BabysitterTest extends PHPUnit_Framework_TestCase{
 		
+	public $charge;
+	
+	public function __construct()
+    {
+        $this->charge = new Babysitter();
+    }
+	
 	public function testCheckStartTimeNoEarlier5p(){
 		$start = new Babysitter();
 		$this->assertTrue($start->checkStartTime(5));		
 	}
 	
 	public function testCheckNoChargeWithSameStartEndBedtime(){
-		$charge = new Babysitter();
-		$this->assertEquals(0, $charge->calcCharge(5, 5, 5));
+		$this->assertEquals(0, $this->charge->calcCharge(5, 5, 5));
 	}
 
 	public function testCheckChargeWithOneHourWorkAndBedtime(){
-		$charge = new Babysitter();
-		$this->assertEquals(12, $charge->calcCharge(5, 6, 6));
+		$this->assertEquals(12, $this->charge->calcCharge(5, 6, 6));
 	}
 	
 }
